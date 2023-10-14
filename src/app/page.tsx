9476aa47264bcb2a48f2913f104d6e93b2633b3b
Mike Lee <guel@me-comp.local>
2023-10-14 23:28:24 +0800

    fix: converted to sign in items to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,24 @@ import Link from 'next/link'
 
 export default function Home() {
   const { data: session } = useSession()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ConnectButton />
-      {!session && (
-        <>
-          Not signed in <br />
-          <button onClick={() => signIn()}>Sign in</button>
-        </>
-      )}
-      {session && (
-        <>
-          Signed in as {session?.user?.email} <br />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )}
+    <main className="flex min-h-screen w-screen flex-col items-center justify-between p-10">
+      <nav className="flex min-w-screen w-screen flex-row justify-between p-10">
+        <ConnectButton />
+        {!session && (
+          <>
+            <p className="self-center">Not signed in </p><br />
+            <button onClick={() => signIn()}>Sign in</button>
+          </>
+        )}
+        {session && (
+          <>
+            <p className="self-center">Signed in as {session?.user?.email}</p><br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        )}
+      </nav>
       <div className="space-x-4">
         <button className="px-4 py-2 bg-blue-500 text-white rounded">
           <Link href="/">Home</Link>
