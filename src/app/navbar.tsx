a45a4c1d37adf0691e0dad874773d8f89ae4b12b
Mike Lee <lee.mikemc@gmail.com>
2023-10-15 01:36:42 +0800

    fix: check authentication using status instead of session

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -3,11 +3,11 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function Navbar() {
-    const { data: session } = useSession()
+    const { data: session, status } = useSession()
     return (
         <nav className="flex flex-row w-screen justify-between p-10">
         <ConnectButton />
-        {!session && (
+        { status === 'unauthenticated' && (
           <>
             <div className="flex flex-row w-1/4 justify-around">
               <p className="">Not signed in</p>
@@ -15,7 +15,7 @@ export default function Navbar() {
             </div>
           </>
         )}
-        {session && (
+        {status === 'authenticated' && (
           <>
             <p className="">Signed in as {session?.user?.email}</p><br />
             <button className="bg-indigo-500 px-2 rounded" onClick={() => signOut()}>Sign out</button>
