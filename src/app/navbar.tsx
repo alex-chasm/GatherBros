c395a114b2329abd88e54d83aeb1b85826d6541c
Mike Lee <lee.mikemc@gmail.com>
2023-10-15 01:34:47 +0800

    fix: made navbar as component

---
+++ src/app/navbar.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+export default function Navbar() {
+    const { data: session } = useSession()
+    return (
+        <nav className="flex flex-row w-screen justify-between p-10">
+        <ConnectButton />
+        {!session && (
+          <>
+            <div className="flex flex-row w-1/4 justify-around">
+              <p className="">Not signed in</p>
+              <button className="bg-indigo-500 px-2 rounded" onClick={() => signIn()}>Sign in</button>
+            </div>
+          </>
+        )}
+        {session && (
+          <>
+            <p className="">Signed in as {session?.user?.email}</p><br />
+            <button className="bg-indigo-500 px-2 rounded" onClick={() => signOut()}>Sign out</button>
+          </>
+        )}
+      </nav>
+
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,15 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
+import Navbar from './navbar'
 
 export default function Home() {
-  const { data: session } = useSession()
+  // const { data: session } = useSession()
 
   return (
     <main className="flex min-h-screen w-screen flex-col items-center justify-between p-10">
-      <nav className="flex flex-row w-screen justify-between p-10">
+      <Navbar />
+      {/* <nav className="flex flex-row w-screen justify-between p-10">
         <ConnectButton />
         {!session && (
           <>
@@ -25,7 +27,7 @@ export default function Home() {
             <button className="bg-indigo-500 px-2 rounded" onClick={() => signOut()}>Sign out</button>
           </>
         )}
-      </nav>
+      </nav> */}
       <div className="space-x-4">
         <button className="px-4 py-2 bg-blue-500 text-white rounded">
           <Link href="/">Home</Link>
